cc9e47b73b5da8d080aba3ecf51d762c780335ce
Caetano Lira <caetanolira@digix.com.br>
2020-02-12 07:03:35 -0400

    Humilde inicio de codigo

---
+++ src/ZionDb.Tests.Unit/ZionDbContextTests.cs
@@ -0,0 +1,46 @@
+using System;
+using Xunit;
+
+namespace ZionDb.Tests.Unit
+{
+    public class ZionDbContextTests
+    {
+        [Fact]
+        public void Should_get_value_by_key()
+        {
+            var sut = new ZionDbContext();
+            const string key = "a key";
+            var valueExpected = new ComplexObjectMock { StringProperty = "a test" };
+            sut.Set(key, valueExpected);
+
+            var valueGetted = sut.Get<ComplexObjectMock>(key);
+
+            Assert.Equal(valueExpected, valueGetted);
+        }
+    }
+
+    public class ComplexObjectMock
+    {
+        public char CharProperty { get; set; }
+        public string StringProperty { get; set; }
+        public short ShortProperty { get; set; }
+        public int IntProperty { get; set; }
+        public long LongProperty { get; set; }
+        public DateTime DateTimeProperty { get; set; }
+        public DateTimeOffset DateTimeOffsetProperty { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            var objeto = (ComplexObjectMock)obj;
+
+            return objeto != null &&
+                   CharProperty == objeto.CharProperty &&
+                   StringProperty == objeto.StringProperty &&
+                   ShortProperty == objeto.ShortProperty &&
+                   IntProperty == objeto.IntProperty &&
+                   LongProperty == objeto.LongProperty &&
+                   DateTimeProperty == objeto.DateTimeProperty &&
+                   DateTimeOffsetProperty == objeto.DateTimeOffsetProperty;
+        }
+    }
+}
---
+++ src/ZionDb/ZionDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace ZionDb
+{
+    public class ZionDbContext
+    {
+        private static readonly HashSet<KeyValuePair<string, string>> Contexto = new HashSet<KeyValuePair<string, string>>();
+
+        public void Set(string key, object value)
+        {
+            Contexto.Add(new KeyValuePair<string, string>(key, JsonConvert.SerializeObject(value)));
+        }
+
+        public T Get<T>(string key)
+        {
+            return JsonConvert.DeserializeObject<T>(Contexto.FirstOrDefault(i => i.Key == key).Value);
+        }
+    }
+}
